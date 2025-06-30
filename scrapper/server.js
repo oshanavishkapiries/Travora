dd605cb9446e0494b9047e790d4162766221b76b
OSHAN AVISHKA <153918928+oshanavishkapiries@users.noreply.github.com>
2025-07-01 02:20:16 +0530

    feat(scrapper) : sandbox-disable

--- scrapper/server.js
+++ scrapper/server.js
@@ -14,7 +14,10 @@ app.get('/', async (req, res) => {
   }
 
   try {
-    const browser = await puppeteer.launch({ headless: 'new' });
+    const browser = await puppeteer.launch({ 
+      headless: 'new',
+      args: ['--no-sandbox', '--disable-setuid-sandbox'] 
+    });
     const page = await browser.newPage();
     
     await page.goto(targetUrl, { waitUntil: 'networkidle2' });
